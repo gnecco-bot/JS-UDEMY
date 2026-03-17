0cd4fcb62d9a2720e5d06532be54609e13bed12d
Lucas <lucasalvesgnecco@gmail.com>
2026-03-16 21:23:47 -0300

    const valores nao mutaveis

---
+++ JS-BASICO/4/index.js
@@ -0,0 +1,16 @@
+const nome = 'João';
+console.log(nome);
+
+const primeiroNumero = 5;
+const primeiroNumeroString = '5';
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+let resultadoDuplicado = resultado * 2 + segundoNumero;
+
+console.log(primeiroNumero + segundoNumero);
+console.log(resultado);
+console.log(resultadoDuplicado);
+
+console.log(typeof primeiroNumero);
+console.log(typeof primeiroNumeroString);
+
