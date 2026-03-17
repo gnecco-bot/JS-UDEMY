397e91807818b4b410ef909380af7ebc75ce8188
Lucas <lucasalvesgnecco@gmail.com>
2026-03-16 21:14:16 -0300

    variavel let

---
+++ JS-BASICO/3/index.js
@@ -0,0 +1,15 @@
+let nome = 'João';
+let nome2 = 'Felipe';
+let nome3 = 'Junior';
+let nome4 = 'Augusto';
+let nome5 = 'Jonas';
+
+let test; // Declarou a variável
+test = 'test da variavel declarada';
+
+console.log('teste', nome);
+console.log('teste2', nome2);
+console.log('teste3', nome3);
+console.log('teste4', nome4);
+console.log('teste5', nome5);
+console.log(test);
