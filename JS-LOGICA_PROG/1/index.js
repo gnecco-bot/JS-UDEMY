479eaf38060baacd6f70c29fdc008d49d70e454b
Lucas <lucasalvesgnecco@gmail.com>
2026-03-21 10:56:44 -0300

    operadores logicos

---
+++ JS-LOGICA_PROG/1/index.js
@@ -0,0 +1,28 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor)
+=== igualdade restrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+
+const comp = 10 > 5;
+console.log(comp)
+const comp2 = 5 >= 5;
+console.log(comp2)
+const comp3 = 1 < 5;
+console.log(comp3)
+const comp4 = -5 <= 5;
+console.log(comp4)
+const comp5 = 5 == '5';
+console.log(comp5)
+const comp6 = 5 === '5';
+console.log(comp6)
+const comp7 = 5 != 4;
+console.log(comp7)
+const comp8 = 5 !== '4';
+console.log(comp8)
