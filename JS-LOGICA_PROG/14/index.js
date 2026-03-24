28fca821585d21a035a347509c8bbc9aedcd5f5f
Lucas <lucasalvesgnecco@gmail.com>
2026-03-24 07:17:39 -0300

    for, indice com arrays

---
+++ JS-LOGICA_PROG/14/index.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 10; i++) {
+    const par = i % 2 === 0 ? 'par' : 'impar';    
+    console.log(i, par);
+    // console.log(`Linha ${i}`)
+}
+
+const frutas = ['Maçâ', 'Pêra', 'Uva'];
+
+for (let i = 0 ;i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i]);
+}
