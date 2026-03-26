1aa9d0958d5150da116fb3bfb0fb3602f7bd6f79
Lucas <lucasalvesgnecco@gmail.com>
2026-03-26 06:46:20 -0300

    try|catch

---
+++ JS-LOGICA_PROG/24/index.js
@@ -0,0 +1,21 @@
+try {
+    console.log(naoExisto);
+} catch (err) {
+    console.log('nao existo nao existe.')
+    console.log(err);
+};
+
+function soma(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        throw('x e y precisiar ser numeros');
+    }
+    return x + y;
+}
+
+try {
+
+    console.log(soma(1,2));
+    console.log(soma('1',2));
+} catch (error) {
+    console.log(error);
+};
