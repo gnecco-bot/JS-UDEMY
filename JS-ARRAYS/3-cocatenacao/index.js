c100f46acf2d95b19eea7746c2071b37df5c331a
Lucas <lucasalvesgnecco@gmail.com>
2026-04-01 07:43:27 -0300

    concatenacao de arrays

--- JS-ARRAYS/3-cocatenacao/index.js
+++ JS-ARRAYS/3-cocatenacao/index.js
@@ -3,6 +3,6 @@
 const a1 = [1, 2, 3];
 const a2 = [4, 5, 6];
 const a3 = a1.concat(a2, [7, 8, 9], 'luz'); // concat() permite colocar strs no meio
-const a4 = [...a1, ...a2]; // spread operation, permite colocar strs no meio
+const a4 = [...a1, ...a2]; // spread operation permite colocar strs no meio
 console.log(a3);
 console.log(a4);
