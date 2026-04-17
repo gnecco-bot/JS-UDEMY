df674b7b727588ed90a5886a7bd25f86aa750d01
Lucas <lucasalvesgnecco@gmail.com>
2026-04-17 19:32:25 -0300

    async await

---
+++ JS-ASSICRONO/3-async await/index.js
@@ -0,0 +1,61 @@
+function rand(min = 0, max = 3) {
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max-min) + min);
+}
+
+// Promise faz execução em pararelo sem pausar o programa para ser executado
+function esperaAi(msg, tempo, cb) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(typeof msg !== 'string') {
+                reject('Cai no erro');
+                return;
+            }; // exibir no bloco catch
+
+            resolve(msg.toUpperCase() + ' - Passei na promise'); // exibir no bloco then
+            return;
+        }, tempo);        
+    });
+}
+
+// esperaAi('Fase 1', rand())
+// .then(valor => {
+//     console.log(valor);
+//     return esperaAi('Fase 2', rand());
+// }).then(fase => {
+//     console.log(fase);
+//     return esperaAi('Fase 3', rand());
+// }).then(fase => {
+//     console.log(fase);
+//     return fase;
+// }).then(fase => {
+//     console.log('Terminamos na fase:', fase);
+// }).catch(e => console.log(e)); 
+
+async function executa() {
+    try {
+        const fase1 = esperaAi('Fase 1', 1000);
+        console.log(fase1);
+
+        setTimeout(function() {
+            console.log('Essa promise estava pendente', fase1)
+        }, 1100);
+
+        const fase2 = await esperaAi('Fase 2', rand());
+        console.log(fase2);
+
+        const fase3 = await esperaAi('Fase 3', rand());
+        // const fase3 = await esperaAi(3440, rand());
+        console.log(fase3);
+
+        console.log('Terminams na fase:', fase3)
+    } catch(e) {
+        console.log(e);
+    }
+}
+executa();
+
+// pending -> pendente
+// fullfilled -> resolvida
+// rejected -> rejeitada
