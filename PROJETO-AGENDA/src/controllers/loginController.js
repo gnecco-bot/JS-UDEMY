f0066b52af98a05b0e445cd4be0171858ed61700
Lucas <lucasalvesgnecco@gmail.com>
2026-05-19 08:11:46 -0300

    login & register

--- PROJETO-AGENDA/src/controllers/loginController.js
+++ PROJETO-AGENDA/src/controllers/loginController.js
@@ -1,3 +1,29 @@
+const Login = require('../models/LoginModel');
+
 exports.index = (req, res) => {
   res.render('login');
+};
+
+exports.register = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.register();
+
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('back');
+      }); 
+      return;
+    }
+
+    req.flash('success', 'Seu usuáro foi criado com sucesso.');
+    req.session.save(function() {
+      return res.redirect('back');
+    }); 
+    return res.send(login.errors);
+  } catch(e) {
+    console.log(e);
+    res.render('404');
+  }
 };
