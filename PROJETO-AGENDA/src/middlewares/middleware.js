29cd43b64ab6899190b7a59ff1e906e03e6004fc
Lucas <lucasalvesgnecco@gmail.com>
2026-05-23 13:57:18 -0300

    cadastrando contato

--- PROJETO-AGENDA/src/middlewares/middleware.js
+++ PROJETO-AGENDA/src/middlewares/middleware.js
@@ -21,3 +21,13 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+exports.loginRequired = (req, res, next) => {
+  if(!req.session.user) {
+    req.flash('errors', 'Você precisa fazer o login.');
+    req.session.save(() => res.redirect('/'));
+    return; 
+  }
+
+  next();
+}
