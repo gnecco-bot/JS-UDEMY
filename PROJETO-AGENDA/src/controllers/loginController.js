d52a8c974b573d5b8e0989646ee7556be2798f9c
Lucas <lucasalvesgnecco@gmail.com>
2026-05-23 13:02:24 -0300

    login usuario completo

--- PROJETO-AGENDA/src/controllers/loginController.js
+++ PROJETO-AGENDA/src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-  res.render('login');
+  if(req.session.user) return res.render('login-logado');
+  return res.render('login');
 };
 
 exports.register = async function(req, res) {
@@ -28,3 +29,33 @@ exports.register = async function(req, res) {
     res.render('404');
   }
 };
+
+exports.login = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect(req.get("Referrer") || '/login/index')
+      }); 
+      return;
+    }
+
+    req.flash('success', 'Você entrou no sistema.');
+    req.session.user = login.user;
+    req.session.save(function() {
+      return res.redirect(req.get("Referrer") || '/login/index')
+    }); 
+
+  } catch(e) {
+    console.log(e);
+    res.render('404');
+  }
+};
+
+exports.logout = function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
+}
