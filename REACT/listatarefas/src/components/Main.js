7d6499647b0de77702a41b11a002a09765d1a1ea
Lucas <lucasalvesgnecco@gmail.com>
2026-05-29 12:04:30 -0300

    visual css da lista de tarefas

--- REACT/listatarefas/src/components/Main.js
+++ REACT/listatarefas/src/components/Main.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './Main.css';
 
+// Form
+import { FaPlus } from 'react-icons/fa';
+
 export default class Main extends Component {
   // constructor(props) {
   //   super(props);
@@ -28,15 +31,17 @@ export default class Main extends Component {
   };
 
   render() {
-    // const { novaTarefa } = this.state;
+    const { novaTarefa } = this.state;
 
     return (
       <div className="main">
-        <h1>Lista de tarefas</h1>
+        <h1>Lista de tarefas {novaTarefa}</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     );
