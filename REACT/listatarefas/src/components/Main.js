eff16b9b20ae920cbd75433df849618938090e9e
Lucas <lucasalvesgnecco@gmail.com>
2026-05-29 11:55:50 -0300

    interface basica com input

---
+++ REACT/listatarefas/src/components/Main.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+export default class Main extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     novaTarefa: '',
+  //   };
+
+  //   this.inputMudou = this.inputMudou.bind(this);
+  // }
+
+  // inputMudou(e) {
+  //   this.setState({
+  //     novaTarefa: e.target.value,
+  //   });
+
+  state = {
+    novaTarefa: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  render() {
+    // const { novaTarefa } = this.state;
+
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+
+        <form action="#">
+          <input onChange={this.handleChange} type="text" />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    );
+  }
+}
